ba8925f0327c77f6947fda5c46be0fa3cb4c0127
iva stival <ivastival@gmail.com>
2023-08-22 11:56:16 -0300

    Create the LCDControl class and initialize it on control Manager

--- control/controlManager.py
+++ control/controlManager.py
@@ -2,38 +2,50 @@ import time
 import yaml
 
 from sensors.temperature.controlDHT22 import controlDHT22
+from lcd.lcdControl import LCDControl
 
 
-def controlManager():
+class controlManager:
+    def __init__(self):
 
-    with open('./config/config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+        with open('./config/config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
 
-    serve_name = config["SERVER"]["SENSOR_LOCATION_NAME"]
-    debug = config["SERVER"]["DEBUG"]
+        #### TEMPERATURE CONFIG DATA ####
+        self.serve_name = config["SERVER"]["SENSOR_LOCATION_NAME"]
+        self.debug = config["SERVER"]["DEBUG"]
 
-    temperature_server_name = config["TEMPERATURE_SERVER"]['NAME']
-    minutes_between_reads = config["TEMPERATURE_SERVER"]["MINUTES_BETWEEN_READS"]
-    unit_type = config["TEMPERATURE_SERVER"]["METRICS_UNIT"]
+        self.temperature_server_name = config["TEMPERATURE_SERVER"]['NAME']
+        self.minutes_between_reads = config["TEMPERATURE_SERVER"]["MINUTES_BETWEEN_READS"]
+        self.unit_type = config["TEMPERATURE_SERVER"]["METRICS_UNIT"]
 
-    dh22_01_name = config["TEMPERATURE_SENSOR_01"]["NAME"]
-    dh22_02_name = config["TEMPERATURE_SENSOR_02"]["NAME"]
+        self.dh22_01_name = config["TEMPERATURE_SENSOR_01"]["NAME"]
+        self.dh22_02_name = config["TEMPERATURE_SENSOR_02"]["NAME"]
 
-    dh22_01_port = config["TEMPERATURE_SENSOR_01"]["PORT"]
-    dh22_02_port = config["TEMPERATURE_SENSOR_02"]["PORT"]
+        self.dh22_01_port = config["TEMPERATURE_SENSOR_01"]["PORT"]
+        self.dh22_02_port = config["TEMPERATURE_SENSOR_02"]["PORT"]
 
-    sensor_DHT22_01 = controlDHT22(
-        dh22_01_port, unit_type, dh22_01_name, debug)
+        self.sensor_DHT22_01 = controlDHT22(
+            self.dh22_01_port, self.unit_type, self.dh22_01_name, self.debug)
 
-    if (debug):
-        print(serve_name)
+        #### LCD CONFIG DATA ####
+        pin_rs = config["LCD"]["PIN_RS"]
+        pin_e = config["LCD"]["PIN_E"]
+        pins_data = config["LCD"]["PINS_DATA"]
 
-    result = {}
-    while True:
-        if (debug):
-            print(temperature_server_name)
+        # INITIALIZE LCD CONTROL
+        self.lcd_control = LCDControl(pin_rs, pin_e, pins_data)
 
-        result[dh22_01_name] = sensor_DHT22_01.run()
-        print(result)
-        print(60*minutes_between_reads)
-        # time.sleep(60*minutes_between_reads)
+        if (self.debug):
+            print(self.serve_name)
+
+    def run(self):
+        result = {}
+        while True:
+            if (self.debug):
+                print(self.temperature_server_name)
+
+            result[self.dh22_01_name] = self.sensor_DHT22_01.run()
+            print(result)
+            print(60*self.minutes_between_reads)
+            # time.sleep(60*minutes_between_reads)
