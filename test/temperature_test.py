c6d0d9cd5c4fada00a8f42af2c5ab10e838db15d
iva stival <ivastival@gmail.com>
2023-08-22 11:27:54 -0300

    add GPIO.BOARD mode

--- test/temperature_test.py
+++ test/temperature_test.py
@@ -1,76 +1,31 @@
-#include <wiringPi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#define MAXTIMINGS	85
-#define DHTPIN		7
-int dht11_dat[5] = { 0, 0, 0, 0, 0 };
- 
-void read_dht11_dat()
-{
-	uint8_t laststate	= HIGH;
-	uint8_t counter		= 0;
-	uint8_t j		= 0, i;
-	float	f; 
- 
-	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
- 
-	pinMode( DHTPIN, OUTPUT );
-	digitalWrite( DHTPIN, LOW );
-	delay( 18 );
-	digitalWrite( DHTPIN, HIGH );
-	delayMicroseconds( 40 );
-	pinMode( DHTPIN, INPUT );
- 
-	for ( i = 0; i < MAXTIMINGS; i++ )
-	{
-		counter = 0;
-		while ( digitalRead( DHTPIN ) == laststate )
-		{
-			counter++;
-			delayMicroseconds( 1 );
-			if ( counter == 255 )
-			{
-				break;
-			}
-		}
-		laststate = digitalRead( DHTPIN );
- 
-		if ( counter == 255 )
-			break;
- 
-		if ( (i >= 4) && (i % 2 == 0) )
-		{
-			dht11_dat[j / 8] <<= 1;
-			if ( counter > 16 )
-				dht11_dat[j / 8] |= 1;
-			j++;
-		}
-	}
- 
-	if ( (j >= 40) &&
-	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-	{
-		f = dht11_dat[2] * 9. / 5. + 32;
-		printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
-	}else  {
-		printf( "Data not good, skip\n" );
-	}
-}
- 
-int main( void )
-{
-	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
- 
-	if ( wiringPiSetup() == -1 )
-		exit( 1 );
- 
-	while ( 1 )
-	{
-		read_dht11_dat();
-		delay( 1000 ); 
-	}
- 
-	return(0);
-}
+# Programa : Sensor de temperatura DHT11 com Raspberry Pi
+# Autor : MakerHero
+
+# Carrega as bibliotecas
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+
+# Define o tipo de sensor
+sensor = Adafruit_DHT.DHT11
+# sensor = Adafruit_DHT.DHT22
+
+GPIO.setmode(GPIO.BOARD)
+
+# Define a GPIO conectada ao pino de dados do sensor
+pino_sensor = 25
+
+# Informacoes iniciais
+print("*** Lendo os valores de temperatura e umidade")
+
+while (1):
+    # Efetua a leitura do sensor
+    umid, temp = Adafruit_DHT.read_retry(sensor, pino_sensor)
+    # Caso leitura esteja ok, mostra os valores na tela
+    if umid is not None and temp is not None:
+        print("Temperatura = {0:0.1f}  Umidade = {1:0.1f}n").format(temp, umid)
+        print("Aguarda 5 segundos para efetuar nova leitura...n")
+        time.sleep(5)
+    else:
+        # Mensagem de erro de comunicacao com o sensor
+        print("Falha ao ler dados do DHT11 !!!")
