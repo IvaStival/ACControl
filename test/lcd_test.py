f336095fa3a820679a93c18c80e5f011f73a98b3
iva stival <ivastival@gmail.com>
2023-08-22 11:30:59 -0300

    add GPIO.BOARD to the function

--- test/lcd_test.py
+++ test/lcd_test.py
@@ -1,7 +1,6 @@
 from RPLCD import CharLCD
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2,
+              pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 lcd.write_string(u'Hello world!')
